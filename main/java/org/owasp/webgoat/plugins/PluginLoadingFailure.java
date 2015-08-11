7dbcc35a7ce697bfef2b1ce723ed7103ce2af2df
Nanne Baars <nbaars@xebia.com>
2015-08-11 21:57:12 +0200

    Moved Maven multiproject setup

--- main/java/org/owasp/webgoat/plugins/PluginLoadingFailure.java
+++
@@ -1,8 +0,0 @@
-package org.owasp.webgoat.plugins;
-
-public class PluginLoadingFailure extends RuntimeException {
-
-    public PluginLoadingFailure(String message, Exception e) {
-        super(message, e);
-    }
-}
